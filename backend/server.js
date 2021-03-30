5f8da6aa1e07b2398ded9582ad5eae0130522c45
NickEllisNME <72147372+NickEllis212@users.noreply.github.com>
2021-03-30 12:22:23 +0100

    first commit

--- backend/server.js
+++ backend/server.js
@@ -22,7 +22,7 @@ app.get('/', (req, res) => {
 });
 
 
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 5000;
 app.listen(port, () =>{
     console.log(`Serve at http://localhost:${port}`);
 });
